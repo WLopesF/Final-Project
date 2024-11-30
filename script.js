1d4715d9f155e4f407a47601e1537edb3fbcc92d
WLopesF <williamlopesfreitas789@gmail.com>
2024-11-30 10:00:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ function verificarRespostas() {
         q2: 'b',
         q3: 'b',
         q4: 'c',
-        q5: 'b'
+        q5: 'b',
+        q6: 'a', 'c',
+        q7: 'c',
+        q8: 'a', 'b',
+        q9: 'a', 'b', 'c',
+        q10: 'a', 'b'
     };
 
     const form = document.getElementById('quizForm');
@@ -20,11 +25,20 @@ function verificarRespostas() {
     }
 
     resultDiv.style.display = 'block';
-    if (pontuacao === 5) {
+    if (pontuacao === 10) {
         resultDiv.innerHTML = `Parabéns! Você acertou ${pontuacao} de ${Object.keys(respostasCorretas).length} perguntas!`;
+        resultDiv.style.color = "darkgreen";
+    } else if (pontuacao === 9) {
+        resultDiv.innerHTML = `Quase lá, não desista! Você acertou ${pontuacao} de ${Object.keys(respostasCorretas).length} perguntas. Continue tentando!`;
         resultDiv.style.color = "green";
-    } else if (pontuacao === 4) {
-        resultDiv.innerHTML = `Quase lá, não desista! Você acertou ${pontuacao} de ${Object.keys(respostasCorretas).length} perguntas. Tente novamente!`;
+    } else if pontuacao in range(6,8) {
+        resultDiv.innerHTML = `Você foi bem! Você acertou ${pontuacao} de ${Object.keys(respostasCorretas).length} perguntas. Tente novamente, você pode melhorar!`;
         resultDiv.style.color = "orange";
-    }
-}
+    } else if (pontuacao === 5) {
+        resultDiv.innerHTML = `Razoável... Estude um pouco mais, pois você acertou ${pontuacao} de ${Object.keys(respostasCorretas).length} perguntas. Volte quando souber mais um pouco!`;
+        resultDiv.innerHTML = "yellow";
+    } else if pontuacao in range(1,4) { 
+        resultDiv.innerHTML = `Você foi mal... Que pena. Recomendo fazer um curso na área de programação. Você acertou ${pontuacao} de ${Object.keys(respostasCorretas).length} perguntas. Tem jeito!;
+        resultDiv.style.color = "orange";
+    } else 
+        
